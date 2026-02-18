cf4505a1c030893c8b63411534fd5b03403075c3
Jackson Galyardt <jgalyardt@gmail.com>
2026-02-18 18:16:15 -0500

    Add SSL configuration and update .gitignore for SSL certificates

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,14 +57,18 @@ if config_env() == :prod do
 
   config :evo, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
+  port = String.to_integer(System.get_env("PORT") || "4000")
+  ssl_keyfile = System.get_env("SSL_KEY_PATH") || "priv/ssl/server.key"
+  ssl_certfile = System.get_env("SSL_CERT_PATH") || "priv/ssl/server.crt"
+
   config :evo, EvoWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+    url: [host: host, port: port, scheme: "https"],
+    https: [
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port,
+      cipher_suite: :strong,
+      keyfile: ssl_keyfile,
+      certfile: ssl_certfile
     ],
     secret_key_base: secret_key_base
 
