24abc88c0829901f152fd4c74bcb55aa390798da
Jackson Galyardt <jgalyardt@gmail.com>
2026-02-17 23:08:54 -0500

    Update production configuration and systemd service for Evo - Uncomment SSL configuration in prod.exs - Modify user, group, and working directory in evo.service

--- config/prod.exs
+++ config/prod.exs
@@ -7,15 +7,9 @@ import Config
 # before starting your production server.
 config :evo, EvoWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Force using SSL in production. This also sets the "strict-security-transport" header,
-# known as HSTS. If you have a health check endpoint, you may want to exclude it below.
-# Note `:force_ssl` is required to be set at compile-time.
-config :evo, EvoWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  exclude: [
-    # paths: ["/health"],
-    hosts: ["localhost", "127.0.0.1"]
-  ]
+# Uncomment to force SSL in production (requires HTTPS to be configured):
+# config :evo, EvoWeb.Endpoint,
+#   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
